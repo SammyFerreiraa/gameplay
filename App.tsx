77137ca15f92486098287e61dfb64516a2d25c5d
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-03 09:19:13 -0300

    :lipstick: feat: background app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import { Rajdhani_700Bold, Rajdhani_500Medium } from '@expo-google-fonts/rajdhani'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import React, { useCallback } from 'react'
+import { StatusBar } from 'react-native'
 
 import { SignIn } from './src/screens/SignIn'
 
+import { Background } from '~/components/Background'
+
+SplashScreen.preventAutoHideAsync()
+
 export default function App() {
-  return <SignIn />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_700Bold,
+    Rajdhani_500Medium,
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+  return (
+    <Background>
+      <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
+      <SignIn onLayout={onLayoutRootView} />
+    </Background>
+  )
 }
