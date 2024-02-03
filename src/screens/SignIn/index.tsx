37914bf9c5faa7a6ff51dad54c1d1a60035dac48
Sammy Ferreira <sammyfe2021@gmail.com>
2024-02-03 09:19:28 -0300

    :sparkles: feat: SignIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Text, View, Image, StatusBar } from 'react-native'
+import { Text, View, Image, LayoutChangeEvent } from 'react-native'
 
 import { styles } from './styles'
 import IllustrationImg from '../../assets/illustration.png'
 
 import { ButtonIcon } from '~/components/ButtonIcon'
 
-export function SignIn() {
+type SignInProps = {
+  onLayout?: ((event: LayoutChangeEvent) => void) | undefined
+}
+
+export function SignIn({ onLayout }: SignInProps) {
   return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
+    <View style={styles.container} onLayout={onLayout}>
       <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`} suas jogatinas {`\n`} facilmente
+          Conecte-se {'\n'} e organize suas {'\n'} jogatinas
         </Text>
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`} favoritos com seus amigos
+          Crie grupos para jogar seus games {'\n'} favoritos com seus amigos
         </Text>
 
         <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
